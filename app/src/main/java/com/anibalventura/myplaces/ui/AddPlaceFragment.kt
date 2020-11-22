47ae26f0206a33c30c6c85a1af0c785538b8cb5e
Anibal Ventura <anibalventura20@gmail.com>
2020-11-21 23:50:11 -0400

    Add gallery and camera picking functionality.

--- app/src/main/java/com/anibalventura/myplaces/ui/AddPlaceFragment.kt
+++ app/src/main/java/com/anibalventura/myplaces/ui/AddPlaceFragment.kt
@@ -1,9 +1,12 @@
 package com.anibalventura.myplaces.ui
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
@@ -17,12 +20,15 @@ import com.afollestad.materialdialogs.datetime.datePicker
 import com.afollestad.materialdialogs.list.listItems
 import com.anibalventura.myplaces.R
 import com.anibalventura.myplaces.databinding.FragmentAddPlaceBinding
+import com.anibalventura.myplaces.utils.Constants.CAMERA_REQUEST_CODE
+import com.anibalventura.myplaces.utils.Constants.GALLERY_REQUEST_CODE
 import com.anibalventura.myplaces.utils.snackBarMsg
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -46,6 +52,29 @@ class AddPlaceFragment : Fragment() {
 
     /** ===================================== Complete fields and save. ===================================== **/
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when {
+            resultCode == Activity.RESULT_OK && requestCode == GALLERY_REQUEST_CODE -> {
+                try {
+                    when {
+                        data!!.data != null -> binding.ivPlaceImage.setImageURI(data.data)
+                    }
+                } catch (e: IOException) {
+                    snackBarMsg(requireView(), e.printStackTrace().toString())
+                }
+            }
+            resultCode == Activity.RESULT_OK && requestCode == CAMERA_REQUEST_CODE -> {
+                try {
+                    binding.ivPlaceImage.setImageBitmap(data!!.extras!!.get("data") as Bitmap)
+                } catch (e: IOException) {
+                    snackBarMsg(requireView(), e.printStackTrace().toString())
+                }
+            }
+        }
+    }
+
     private fun pickDate() {
         binding.etDate.setOnClickListener {
             MaterialDialog(requireContext(), BottomSheet(LayoutMode.WRAP_CONTENT)).show {
@@ -65,16 +94,38 @@ class AddPlaceFragment : Fragment() {
                 title(R.string.add_place_add_image)
                 listItems(items = actionsItems) { _, index, _ ->
                     when (index) {
-                        0 -> chooseImageFromGallery()
-                        1 -> snackBarMsg(requireView(), "Test 1")
+                        0 -> pickImageFromGallery()
+                        1 -> takePhotoFromCamera()
                     }
                 }
             }
         }
     }
 
-    private fun chooseImageFromGallery() {
+    private fun takePhotoFromCamera() {
+        Dexter.withContext(requireContext()).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+        ).withListener(object : MultiplePermissionsListener {
 
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+                    val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    startActivityForResult(takePhotoIntent, CAMERA_REQUEST_CODE)
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: MutableList<PermissionRequest>, token: PermissionToken
+            ) {
+                showPermissionDeniedDialog()
+            }
+
+        }).onSameThread().check()
+    }
+
+    private fun pickImageFromGallery() {
         Dexter.withContext(requireContext()).withPermissions(
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -82,31 +133,39 @@ class AddPlaceFragment : Fragment() {
 
             override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
                 if (report!!.areAllPermissionsGranted()) {
-                    snackBarMsg(requireView(), getString(R.string.permission_granted_storage))
+                    val pickImageIntent = Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    )
+                    startActivityForResult(pickImageIntent, GALLERY_REQUEST_CODE)
                 }
             }
 
             override fun onPermissionRationaleShouldBeShown(
                 permissions: MutableList<PermissionRequest>, token: PermissionToken
             ) {
-                MaterialDialog(requireContext()).show {
-                    title(R.string.dialog_permission_denied)
-                    message(R.string.dialog_permission_denied_msg)
-                    negativeButton(R.string.dialog_cancel)
-                    positiveButton(R.string.dialog_go_to_settings) {
-                        try {
-                            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                            intent.data = Uri.fromParts("package", "packageName", null)
-                            startActivity(intent)
-                        } catch (e: Exception) {
-                            snackBarMsg(requireView(), e.printStackTrace().toString())
-                        }
-                    }
-                }
+                showPermissionDeniedDialog()
             }
 
         }).onSameThread().check()
+    }
 
+    private fun showPermissionDeniedDialog() {
+        MaterialDialog(requireContext()).show {
+            title(R.string.dialog_permission_denied)
+            message(R.string.dialog_permission_denied_msg)
+            negativeButton(R.string.dialog_cancel)
+            positiveButton(R.string.dialog_go_to_settings) {
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    intent.data = Uri.fromParts("package", "packageName", null)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    snackBarMsg(requireView(), e.printStackTrace().toString())
+                }
+            }
+        }
     }
 
     private fun savePlace() {
--- app/src/main/java/com/anibalventura/myplaces/utils/Constants.kt
+++ app/src/main/java/com/anibalventura/myplaces/utils/Constants.kt
@@ -2,4 +2,7 @@ package com.anibalventura.myplaces.utils
 
 object Constants {
 
+    // Permissions
+    const val GALLERY_REQUEST_CODE = 1
+    const val CAMERA_REQUEST_CODE = 2
 }
